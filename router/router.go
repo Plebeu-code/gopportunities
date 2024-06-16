9b22d8e8bbdf8ee934a66b1629e034870bc72e62
Plebeu-Code <astronauta2027@gmail.com>
2024-06-16 10:12:18 -0300

    documenting route

--- router/router.go
+++ router/router.go
@@ -3,13 +3,16 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// Inicializa o Router utilizan as configurações Defaults do gin
 	router := gin.Default()
 
+	// Definindo uma Rota
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Activated",
 		})
 	})
 
-	router.Run()
+	// Setando a porta da API
+	router.Run(":8080")
 }
