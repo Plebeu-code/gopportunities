40cb4677d63328ee610e0be2b6886f4c1607127d
Plebeu-Code <astronauta2027@gmail.com>
2024-06-16 12:24:23 -0300

    :sparkles: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/Plebeu-code/gopportunities/router"
+import (
+	"github.com/Plebeu-code/gopportunities/config"
+	"github.com/Plebeu-code/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Config
+	err := config.Init()
+	if err != nil {
+		// panic(err)
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
 	//Initialize Router
 	router.Initialize()
 }
