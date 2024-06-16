9cca47fb0f8e550f8da5d02da91a26b47ebabfae
Plebeu-Code <astronauta2027@gmail.com>
2024-06-16 11:15:28 -0300

    :bricks: update handler structure to separate package

--- router/routes.go
+++ router/routes.go
@@ -1,8 +1,7 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/Plebeu-code/gopportunities/handler"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,34 +9,14 @@ func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 
 	{
-		v1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET oppening",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
 
-		v1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "POST oppening",
-			})
-		})
+		v1.POST("/opening", handler.CreateOpeningHandler)
 
-		v1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "DELETE oppening",
-			})
-		})
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 
-		v1.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "PUT oppening",
-			})
-		})
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
 
-		v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET oppening",
-			})
-		})
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
